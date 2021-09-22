a395479b536618104cd5116727a0ff0fbf498ff6
Xinyu Chen <chenxy346@mail2.sysu.edu.cn>
2021-09-22 18:56:47 -0400

    Update table_header.js

--- table_header.js
+++ table_header.js
@@ -8,17 +8,17 @@ document.write("<h1>Xinyu Chen</h1>");
 document.write("<b>Ph.D. Candidate</b>");
 document.write("<br>");
 
-document.write("<a href="https:\/\/www.umontreal.ca\/en\/"">University of Montreal</a></b>");
+document.write("<a href="https:\/\/www.umontreal.ca\/en\/"">University of Montreal<\/a></b>");
 
 document.write("<br>");
 document.write("<br>");
 document.write("<b>Contact info:</b><br>");
-document.write(" Montreal (QC) Canada. <br>");
-document.write("E-mail: chenxy346@gmail.com<br>");
+// document.write(" Montreal (QC) Canada. <br>");
+document.write("E-mail: chenxy346@gmail.com <br>");
 document.write("<br>");
 document.write("&nbsp;");
-document.write("<a href=\"https:\/\/scholar.google.com\/citations?user=mCrW04wAAAAJ&hl=en\"><img src=\"Images/scholar.png\" alt=\"Google Scholar\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
-document.write("<a href=\"https:\/\/github.com\/xinychen"><img src=\"Images/GitHub-Mark-120px-plus.png\" alt=\"GitHub" width=\"25\" border=\"0\" height=\"25\"> </a> &nbsp;");
+document.write("<a href=\"https:\/\/scholar.google.com\/citations?user=mCrW04wAAAAJ&hl\"><img src=\"Images/scholar.png\" alt=\"Google Scholar\" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
+document.write("<a href=\"https:\/\/github.com\/xinychen"><img src=\"Images/GitHub-Mark-120px-plus.png\" alt=\"GitHub" width=\"25\" border=\"0\" height=\"25\"> <\/a> &nbsp;");
 document.write("<a href=\"https:\/\/twitter.com\/chenxy346\/\"> <img src=\"Images\/twitter.png\" alt=\"Twitter\"  border=\"0\" height=\"25\"><\/a><br>");
 document.write("<\/td>");
 document.write("<\/tr>");
