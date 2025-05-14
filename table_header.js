8a31e621e4180302fbbc2e6a7408171a4c0f154f
Xinyu Chen <chenxy346@mail2.sysu.edu.cn>
2025-05-14 18:56:48 -0400

    Update table_header.js

--- table_header.js
+++ table_header.js
@@ -7,9 +7,9 @@ document.write("width=\"180\" height=\"225\"> <\/td>");
 document.write("<td>");
 document.write("<h1>Xinyu Chen</h1>");
 document.write("<br>");
-document.write("Ph.D., <a href=\"https:\/\/www.umontreal.ca\/en\">University of Montreal<\/a>, Canada");
+document.write("Postdoctoral Associate, <a href=\"https:\/\/www.mit.edu\/\">MIT<\/a>, USA");
 document.write("<br>");
-document.write("Postdoctoral Associate at <a href=\"https:\/\/www.mit.edu\/\">MIT<\/a>, USA");
+document.write("Ph.D., <a href=\"https:\/\/www.umontreal.ca\/en\">University of Montreal<\/a>, Canada");
 document.write("<br>");
 
 // document.write("<a href=\"https:\/\/www.umontreal.ca\/en\">University of Montreal<\/a></b>");
